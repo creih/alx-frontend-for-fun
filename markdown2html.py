dc7a65567c531d8f5ec56610f7b767cd397fdf9c
creih <iherr.9@gmail.com>
2024-08-11 21:41:09 +0200

    added the completion foor task 1

--- markdown2html.py
+++ markdown2html.py
@@ -1,37 +1,25 @@
 #!/usr/bin/python3
-"""module to convert markdown to html."""
+"""task module for markdown to html conversion"""
 import sys
 import os
 import re
 
 
-def parse_markdown_line(line, in_list):
+def parse_markdown_line(line):
     """
-    Parses a single line of markdown and converts it to HTML.
-    Handles headings and unordered lists.
+    Parses single line of markdown and convert it to HTML if matches h pattern
     """
-    heading_match = re.match(r'^(#{1,5}) (.+)$', line)
-    list_item_match = re.match(r'^- (.+)$', line)
+    heading_match = re.match(r'^(#{1,6}) (.+)$', line)
 
     if heading_match:
         heading_level = len(heading_match.group(1))
         heading_text = heading_match.group(2)
-        return f"<h{heading_level}>{heading_text}</h{heading_level}>", in_list
-
-    if list_item_match:
-        list_item = list_item_match.group(1)
-        if not in_list:
-            return f"<ul>\n<li>{list_item}</li>", True
-        return f"<li>{list_item}</li>", in_list
-
-    if in_list:
-        return f"</ul>\n{line}", False
-
-    return line, in_list
+        return f"<h{heading_level}>{heading_text}</h{heading_level}>"
+    return line
 
 
 def main():
-    """main module for our functionalities"""
+    """Check if the number of arguments is less than 2"""
     if len(sys.argv) < 3:
         print(
             "Usage: ./markdown2html.py README.md README.html",
@@ -50,13 +38,9 @@ def main():
         markdown_lines = md_file.readlines()
 
     html_lines = []
-    in_list = False
     for line in markdown_lines:
-        html_line, in_list = parse_markdown_line(line.strip(), in_list)
+        html_line = parse_markdown_line(line.strip())
         html_lines.append(html_line)
-
-    if in_list:
-        html_lines.append("</ul>")
 
     with open(output_file, 'w') as out_file:
         out_file.write("<html><body>\n")
