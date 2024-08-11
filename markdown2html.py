c2ba7c83ee17b9b46e7cee194185152460ff424e
creih <iherr.9@gmail.com>
2024-08-12 00:01:50 +0200

    doing the lists

--- markdown2html.py
+++ markdown2html.py
@@ -1,30 +1,50 @@
-#!/usr/bin/python3
-"""task module for markdown to html conversion"""
+#!/usr/bin/env python3
+"""this module file is for markdown to html conversion"""
 import sys
 import os
 import re
 
 
-def parse_markdown_line(line):
+def parse_markdown_line(line, in_list):
     """
-    Parses single line of markdown and convert it to HTML if matches h pattern
+    Parses a single line of markdown and converts it to HTML.
+    Handles headings and unordered lists.
     """
     heading_match = re.match(r'^(#{1,6}) (.+)$', line)
+    list_item_match = re.match(r'^- (.+)$', line)
 
     if heading_match:
-        heading_level = len(heading_match.group(1))
-        heading_text = heading_match.group(2)
-        return f"<h{heading_level}>{heading_text}</h{heading_level}>"
-    return line
+        if in_list:
+            in_list = False
+            return (f"</ul>\n<h{len(heading_match.group(1))}>"
+                    f"{
+                        heading_match.group(2)
+                        }</h{len(heading_match.group(1))}>",
+                    in_list)
+
+        return (f"<h{len(heading_match.group(1))}>"
+                f"{heading_match.group(2)}</h{len(heading_match.group(1))}>",
+                in_list)
+
+    if list_item_match:
+        if not in_list:
+            in_list = True
+            return f"<ul>\n<li>{list_item_match.group(1)}</li>", in_list
+
+        return f"<li>{list_item_match.group(1)}</li>", in_list
+
+    if in_list:
+        in_list = False
+        return f"</ul>\n{line}", in_list
+
+    return line, in_list
 
 
 def main():
-    """Check if the number of arguments is less than 2"""
+    """main function for markdown to html conversion"""
     if len(sys.argv) < 3:
-        print(
-            "Usage: ./markdown2html.py README.md README.html",
-            file=sys.stderr
-            )
+        print("Usage: ./markdown2html.py README.md README.html",
+              file=sys.stderr)
         sys.exit(1)
 
     markdown_file = sys.argv[1]
@@ -38,9 +58,13 @@ def main():
         markdown_lines = md_file.readlines()
 
     html_lines = []
+    in_list = False
     for line in markdown_lines:
-        html_line = parse_markdown_line(line.strip())
+        html_line, in_list = parse_markdown_line(line.strip(), in_list)
         html_lines.append(html_line)
+
+    if in_list:
+        html_lines.append("</ul>")
 
     with open(output_file, 'w') as out_file:
         out_file.write("\n".join(html_lines))
